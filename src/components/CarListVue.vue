5cfdf4ab191a1f751171958168cd23d099e6dfb4
strizhko_aa <strizhko_aa@it-thematic.ru>
2023-01-10 16:58:47 +0300

    лагает список моделей даже при захардкоженных данных

--- src/components/CarListVue.vue
+++ src/components/CarListVue.vue
@@ -1,22 +1,60 @@
 <template>
   <div class="car-list-wrapper">
-    ASD
+    <div>
+      <p v-for="(car, carIndex) in cars" :key="carIndex">{{ car }}</p>
+    </div>
+    <v-dialog width="500px" v-model="isCreateCar">
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" v-bind="props">Добавить авто</v-btn>
+      </template>
+      <CreateCarVue/>
+    </v-dialog>
+    <!-- <v-btn @click="getCars">get data</v-btn> -->
+    <v-btn @click="addTestData">add data</v-btn>
   </div>
 </template>
 
 <script>
-import { onMounted } from 'vue'
+import { onMounted, ref as vueRef } from 'vue'
+import { getDatabase, ref, onValue, set } from "firebase/database";
+import CreateCarVue from './CreateCarVue.vue';
 
 export default {
-  name: 'car-list',
-  setup () {
-    onMounted(() => {
-      getCars()
-    })
+    name: "car-list",
+    components: { CreateCarVue },
+    setup () {
+        let isCreateCar = vueRef(false);
+        const database = getDatabase();
+        let carRef = ref(database, "cars");
 
-    function getCars () {
-      
+        onMounted(() => {
+            getCars();
+        });
+
+
+        function addTestData() {
+          set(carRef, {
+              Model: "XX999",
+              Mark: "BMW",
+              Number: 999,
+              Region: 888
+          });
+        }
+
+        let cars = vueRef([]);
+        function getCars() {
+          onValue(carRef, (snapshot) => {
+            let snapshotVal = snapshot.val();
+            console.log(snapshotVal);
+            if (Array.isArray(snapshotVal)) {
+              cars.value = snapshotVal;
+            }
+            else {
+              cars.value.push(snapshotVal);
+            }
+          })
+        }
+        return { addTestData, getCars, cars, isCreateCar }
     }
-  }
 }
 </script>
